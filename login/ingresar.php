c996d8e3f4770d02eced16b9198e5ac622fa6ed5
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2017-10-01 12:12:38 -0400

    Logi

--- login/ingresar.php
+++ login/ingresar.php
@@ -20,18 +20,12 @@ if($cantidadfilas==0){//incorrecto
 	$fila=array_shift($registro);
 	$fecha=date("Y-m-d");
 	$hora=date("H:i:s");
-	$ip=$_SERVER['REMOTE_ADDR'];
-	$agente=$_SERVER['HTTP_USER_AGENT'];
+
 	
 	$_SESSION['login']=1;
 	$_SESSION['cod_usuario']=$fila['codigo_usuario'];
 	$_SESSION['nivelusuario']=$fila['nivelusuario'];
 	
-	$valores=array("nivelusuario"=>$fila['nivelusuario'],
-				   "ip"=>"$ip",
-				   "agente"=>"$agente"
-	);
-	insertar("login",$valores);
 	header("Location:../index.php");	
 }
 ?>
