3a4173cc137a02c2cad8c0879fb88341b8aa5e78
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2017-10-02 11:54:56 -0400

    Index

--- index.php
+++ index.php
@@ -1,10 +1,39 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Documento sin título</title>
-</head>
+<?php
+$ruta="";
+session_start();
+///print_r($_SESSION);
+include_once("login/verificar.php");
+$titulo="Inicio";
+$_SESSION['idmenu']=0;
+$_SESSION['subm']=0;
+?>
+<?php include_once("cabecerahtml.php"); ?>
+<link href="css/default/default.css" type="text/css" rel="stylesheet" />
+<link href="css/light/light.css" type="text/css" rel="stylesheet" />
+<link href="css/nivo-slider.css" type="text/css" rel="stylesheet" />
+<script language="javascript" src="js/jquery.nivo.slider.pack.js"></script>
+<script type="text/javascript">
+    $(window).load(function() {
+        $('#slider').nivoSlider();
+    });
+</script>
+<?php include_once("cabecera.php");?>
+<div class="grid_6">
+    <div class="contenido">
+    	<div class="theme-light">
+    	<div id="slider" class="nivoSlider">
+                <img src="imagenes/ropa2.jpg" width="440" style="border-radius: 5px;"/>
+                
+		</div>
+        </div>
+    </div>
+</div>
+<div class="grid_6">
+    <div class="contenido textoinicio">
+    	<h3>Visión</h3>
+    El vídeo proporciona una manera eficaz para ayudarle a demostrar el punto. Cuando haga clic en Vídeo en línea, puede pegar el código para insertar del vídeo que desea agregar.
+También puede escribir una palabra clave para buscar en línea el vídeo que mejor se adapte a su documento.Para otorgar a su documento un aspecto profesional, Word proporciona encabezados, pies de página, páginas de portada y diseños de cuadro de texto que se complementan entre sí.
 
-<body>
-</body>
-</html>
+    </div>
+</div>
+<?php include_once("piepagina.php");?>
