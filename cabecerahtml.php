9777088595bd482c3829a7b8976c0393c3d861f9
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2017-10-03 14:39:06 -0400

    Cabecera Corregido

--- cabecerahtml.php
+++ cabecerahtml.php
@@ -17,15 +17,9 @@ $us=array_shift($us);
 <meta name="keywords" content="" />
 <title><?php echo $titulo;?></title>
 <link href="<?php echo $ruta;?>css/960/960.css" type="text/css" rel="stylesheet" media="screen">
-<link href="<?php echo $ruta;?>css/tabcontent.css" rel="stylesheet" type="text/css"/>
+
 <link href="<?php echo $ruta;?>css/core.css" type="text/css" rel="stylesheet" media="screen">
 <link href="<?php echo $ruta;?>css/menu/styles.css" type="text/css" rel="stylesheet">
-<link href="<?php echo $ruta;?>css/select2.css" type="text/css" rel="stylesheet">
-<link rel="shortcut icon" type="image/x-icon" href="<?php echo $folder; ?>imagenes/favicon.ico" />
-<script language="javascript" type="text/javascript" src="<?php echo $folder;?>js/jquery.js"></script>
-<script language="javascript" type="text/javascript" src="<?php echo $folder;?>js/tabcontent.js"></script> 
-<script language="javascript" type="text/javascript" src="<?php echo $folder;?>js/jquery.form.js"></script>
-<script language="javascript" type="text/javascript" src="<?php echo $folder;?>js/select2.min.js"></script>
-<script language="javascript" type="text/javascript" src="<?php echo $folder;?>js/select2_locale_es.js"></script>
 
-<script language="javascript" type="text/javascript" src="<?php echo $folder;?>js/busquedas/busquedas.js"></script>
+<link rel="shortcut icon" type="image/x-icon" href="<?php echo $ruta; ?>imagenes/favicon.ico" />
+<script language="javascript" type="text/javascript" src="<?php echo $ruta;?>js/jquery.js"></script>
