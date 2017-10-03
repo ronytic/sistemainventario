4cac1d973a490f67ddb989882ca409023cc8cdde
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2017-10-03 13:53:05 -0400

    Ver Venta

--- venta/ver.php
+++ venta/ver.php
@@ -1,11 +1,88 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Documento sin título</title>
-</head>
+<?php
+$ruta="../";
+include_once("../basededatos/bd.php");
+include_once("../basededatos/config.php");
 
-<body>
-asd
-</body>
-</html>
+
+if(isset($_GET['codigo'])){
+    $codigoseparado=explode("-",$_GET['codigo']);
+    $codproducto=$codigoseparado[3];
+//    echo $codproducto;
+    
+}else{
+$codproducto=$_GET['codproducto'];
+}
+$producto=seleccionar("producto","*","codproducto=$codproducto");
+$producto=array_shift($producto);
+
+
+$entradas=seleccionar("entrada","SUM(cantidad)-SUM(devuelto) as Total","codproducto=$codproducto");
+$entradas=array_shift($entradas);
+$entradasTotales=$entradas['Total'];
+
+$salidas=seleccionar("venta","sum(cantidad) as Total","codproducto=$codproducto");
+$salidas=array_shift($salidas);
+$salidasTotales=$salidas['Total'];
+
+$stock=$entradasTotales-$salidasTotales;
+
+include_once("../cabecerahtml.php");
+?>
+<?php include_once("../cabecera.php");?>
+
+<div class="prefix_3 grid_6">
+  <div class="titulo">Datos del Producto</div>
+   <form action="guardar.php" method="post" enctype="multipart/form-data">
+   <input type="hidden" name="codproducto" value="<?php echo $codproducto?>">
+    <table class="tablareg">
+       <tr>
+           <td>Nombre Producto: <?php echo $producto['nombre']?>
+           
+           </td>
+           <td rowspan="5">Foto del Producto: <br><img src="../imagenes/productos/<?php echo $producto['foto']?>" width="200">
+           </td>
+       </tr>
+       <tr>
+           <td>Marca Producto: <?php echo $producto['marca']?>
+           
+           </td>
+       </tr>
+       <tr>
+           <td>Precio Producto: <?php echo $producto['precio']?>
+           </td>
+       </tr>
+       <tr>
+           <td>Talla Producto: <?php echo $producto['talla']?>
+           </td>
+       </tr>
+       <tr>
+           <td>Detalle Producto: <?php echo $producto['detalle']?>
+           </td>
+       </tr>
+       <tr>
+           <td colspan="2">
+           <strong><h3>STOCK DE INVENTARIO: <?php echo $stock;?></h3></strong>
+           </td>
+       </tr>
+       <tr>
+           <td colspan="2">
+           CANTIDAD DE VENTA: <br>
+           <input type="number" name="cantidad" min="0" max="<?php echo $stock;?>" value="0">
+           </td>
+       </tr>
+       <tr>
+           <td colspan="2">
+           FECHA DE VENTA: <br>
+           <input type="date" name="fechaventa" value="<?php echo date("Y-m-d");?>" required>
+           </td>
+       </tr>
+       <tr>
+           <td>
+           <input type="submit"  value="Guardar">
+           </td>
+       </tr>
+   </table>
+    </form>
+</div>
+
+<?php include_once("../piepagina.php");?>
