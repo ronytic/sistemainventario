8ef4c25db4a56a772716143ca1cf3c9e42d662a2
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2017-10-06 02:28:56 -0400

    INDE

--- index.php
+++ index.php
@@ -8,15 +8,6 @@ $_SESSION['idmenu']=0;
 $_SESSION['subm']=0;
 ?>
 <?php include_once("cabecerahtml.php"); ?>
-<link href="css/default/default.css" type="text/css" rel="stylesheet" />
-<link href="css/light/light.css" type="text/css" rel="stylesheet" />
-<link href="css/nivo-slider.css" type="text/css" rel="stylesheet" />
-<script language="javascript" src="js/jquery.nivo.slider.pack.js"></script>
-<script type="text/javascript">
-    $(window).load(function() {
-        $('#slider').nivoSlider();
-    });
-</script>
 <?php include_once("cabecera.php");?>
 <div class="grid_6">
     <div class="contenido">
@@ -30,9 +21,37 @@ $_SESSION['subm']=0;
 </div>
 <div class="grid_6">
     <div class="contenido textoinicio">
-    	<h3>Visión</h3>
-    El vídeo proporciona una manera eficaz para ayudarle a demostrar el punto. Cuando haga clic en Vídeo en línea, puede pegar el código para insertar del vídeo que desea agregar.
-También puede escribir una palabra clave para buscar en línea el vídeo que mejor se adapte a su documento.Para otorgar a su documento un aspecto profesional, Word proporciona encabezados, pies de página, páginas de portada y diseños de cuadro de texto que se complementan entre sí.
+    	<h3>asdasd asd as asd </h3>
+    asdas dasd 
+    asd asd
+    a sd
+    asd 
+    as d
+    as das
+     as
+     d 
+     asd 
+     asd 
+     sad
+     <br>
+     asd
+     as
+     d
+     asd<br>
+     qwe
+     qwe
+     qw<br>
+     e
+     qwe
+     qw
+     eq
+     we
+     qw
+     eqw
+     e
+     qwe
+     
+     
 
     </div>
 </div>
