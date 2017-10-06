473ba9e680758eefb3421d179f3b8290c7facb6f
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2017-10-06 02:28:38 -0400

    Eliminado usuarios

--- usuarios/eliminar.php
+++
@@ -1,6 +0,0 @@
-<?php
-$codigo_usuario=$_GET['codigo_usuario'];
-include_once("../login/verificar.php");
-include_once("../basedatos/bd.php");
-eliminar("usuario","codigo_usuario=$codigo_usuario");
-?>
